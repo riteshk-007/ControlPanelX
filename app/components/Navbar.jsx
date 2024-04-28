644330c51c429f351c9e1e5d3b6913c26f7c85af
Riteshk_007 <codeshorts007@gmail.com>
2024-04-28 14:00:41 +0530

    Add Navbar component to RootLayout in layout.js Update dependencies in package.json

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,160 @@
+"use client";
+import { AlignJustify, X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleMenuClick = () => {
+    setShow(!show);
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setShow(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <header className="w-full">
+      <div className="mx-auto  py-2 px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center md:gap-12">
+            <Link
+              href="/"
+              onClick={() => setShow(false)}
+              className="bg-white p-2 rounded-full shadow-md "
+            >
+              <Image src={"/icon.png"} width={40} height={40} alt={"logo"} />
+            </Link>
+          </div>
+
+          <div
+            className={`md:block ${
+              show
+                ? "block md:hidden absolute top-2 left-1 w-[98%]  bg-gray-800 p-4 rounded-2xl shadow-xl z-10"
+                : "hidden"
+            }`}
+          >
+            <nav aria-label="Global">
+              <ul className="flex w-full md:w-auto flex-col md:flex-row justify-start  md:justify-evenly items-start md:items-center gap-6 text-sm">
+                <Link
+                  href="/"
+                  className="bg-white p-2 rounded-full shadow-md md:hidden block"
+                  onClick={() => setShow(false)}
+                >
+                  <Image
+                    src={"/icon.png"}
+                    width={40}
+                    height={40}
+                    alt={"logo"}
+                  />
+                </Link>
+                <li>
+                  <Link
+                    className="text-gray-200 transition-all  hover:text-gray-200/75"
+                    onClick={() => setShow(false)}
+                    href="/"
+                  >
+                    Home
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    className="text-gray-200 transition-all  hover:text-gray-200/75"
+                    onClick={() => setShow(false)}
+                    href="/"
+                  >
+                    About
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    className="text-gray-200 transition-all  hover:text-gray-200/75"
+                    onClick={() => setShow(false)}
+                    href="/"
+                  >
+                    {" "}
+                    Services{" "}
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    className="text-gray-200 transition-all  hover:text-gray-200/75"
+                    onClick={() => setShow(false)}
+                    href="/"
+                  >
+                    Contact
+                  </Link>
+                </li>
+
+                <Link
+                  className="rounded-md md:hidden block w-full text-center text-sm font-medium"
+                  href="/"
+                  onClick={() => setShow(false)}
+                >
+                  <Button
+                    variant="outline"
+                    className="text-black bg-gray-100 shadow w-full"
+                  >
+                    Login
+                  </Button>
+                </Link>
+
+                <button
+                  onClick={handleMenuClick}
+                  className=" md:hidden block text-white mx-2  rounded-md font-semibold text-sm cursor-pointer shadow-md  absolute right-2 top-4"
+                >
+                  {show ? (
+                    <X size={28} strokeWidth={2.5} />
+                  ) : (
+                    <AlignJustify size={28} strokeWidth={2.5} />
+                  )}
+                </button>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="text-black bg-gray-100 shadow"
+                >
+                  Login
+                </Button>
+              </Link>
+            </div>
+
+            <div className="block md:hidden">
+              <button
+                onClick={handleMenuClick}
+                className=" md:hidden block text-white mx-2  rounded-md font-semibold text-sm cursor-pointer shadow-md"
+              >
+                {show ? (
+                  <X size={28} strokeWidth={2.5} />
+                ) : (
+                  <AlignJustify size={28} strokeWidth={2.5} />
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="bg-[#111] text-white">{children}</div>
+        <div className="bg-[#111] text-white">
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   );
