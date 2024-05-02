1a0b90d18a498878aa025e6016dc50a86a7bc8db
Riteshk_007 <codeshorts007@gmail.com>
2024-05-02 21:00:42 +0530

    create Single User API

---
+++ app/api/all-users/[id]/route.js
@@ -0,0 +1,30 @@
+import prisma from "@/DB/db.config";
+import { NextResponse } from "next/server";
+
+export const GET = async (_, { params }) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: params.id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        phone: true,
+        data: true,
+      },
+    });
+    if (!user)
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+    return NextResponse.json({
+      status: 200,
+      data: user,
+      message: "User found",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
