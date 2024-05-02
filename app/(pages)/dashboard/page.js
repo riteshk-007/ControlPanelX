221a28c3f5187958c76d9f6afcb0e9d6bfb7f515
Riteshk_007 <codeshorts007@gmail.com>
2024-05-02 17:01:52 +0530

    Update dashboard page component, add user login component, and format phone number in LoginUser component

--- app/(pages)/dashboard/page.js
+++ app/(pages)/dashboard/page.js
@@ -1,7 +1,9 @@
+"use client";
 import LoginUser from "@/app/components/LoginUser";
-import React from "react";
+import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
+  const { data: session } = useSession();
   return (
     <div className="w-full p-3 md:p-5 flex flex-col">
       <h1 className="text-xl md:text-3xl font-bold text-center md:text-start">
@@ -10,7 +12,7 @@ const Dashboard = () => {
       <p className="md:text-lg text-sm text-center md:text-start">
         🎉 Welcome to the User Management Control Panel X 🚀
       </p>
-      <LoginUser />
+      <LoginUser session={session} />
     </div>
   );
 };
--- app/components/LoginUser.jsx
+++ app/components/LoginUser.jsx
@@ -1,4 +1,3 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,11 +6,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useSession } from "next-auth/react";
 import React from "react";
 
-const LoginUser = () => {
-  const { data: session } = useSession();
+const LoginUser = ({ session }) => {
+  const phone = session?.user?.phone;
+  const formattedPhone = phone
+    ? `${phone.slice(0, 4)} ${phone.slice(4, 8)} ${phone.slice(8)}`
+    : "0000 0000 00";
   return (
     <Card
       className="sm:col-span-2 bg-black text-white border border-gray-700 w-full sm:min-w-96  mt-5 md:w-1/2 lg:w-1/3"
@@ -25,11 +26,11 @@ const LoginUser = () => {
           </span>
         </CardTitle>
         <CardDescription className="max-w-lg text-gray-300 leading-relaxed gap-2">
-          You are logged in as {session?.user?.email}
+          You are logged in as {session?.user?.email || "user email address"}.
         </CardDescription>
       </CardHeader>
       <CardFooter>
-        <Button>+91 {session?.user?.phone.split("5").join(" ")}</Button>
+        <Button>+91 {formattedPhone}</Button>
       </CardFooter>
     </Card>
   );
