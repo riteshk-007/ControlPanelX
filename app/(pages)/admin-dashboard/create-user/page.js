80b59c231c77a93c1e4d3dffe2983583ad227cb5
Riteshk_007 <codeshorts007@gmail.com>
2024-05-06 17:47:12 +0530

    feat: Add "Create User" page to Admin Dashboard

--- app/(pages)/admin-dashboard/create-user/page.js
+++ app/(pages)/admin-dashboard/create-user/page.js
@@ -1,7 +1,16 @@
-import React from "react";
+import CreateUserform from "@/app/components/CreateUserform";
+import { User } from "lucide-react";
 
 const CreateUser = () => {
-  return <div></div>;
+  return (
+    <div className="w-full flex-col flex p-5">
+      <h1 className="text-2xl font-bold mb-5 flex gap-2 items-center ">
+        <User />
+        Create User
+      </h1>
+      <CreateUserform />
+    </div>
+  );
 };
 
 export default CreateUser;
---
+++ app/components/CreateUserform.jsx
@@ -0,0 +1,255 @@
+"use client";
+import { useForm, useWatch } from "react-hook-form";
+
+const CreateUserform = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm();
+
+  const watchedFields = useWatch({
+    control,
+    name: ["domains", "hosting", "dashboard", "cpanel"],
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className=" mx-auto">
+      <div className="mb-4">
+        <label htmlFor="name" className="block font-bold mb-2">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
+          {...register("name", { required: "Name is required" })}
+        />
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="email" className="block font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Invalid email address",
+            },
+          })}
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="password" className="block font-bold mb-2">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
+          {...register("password", { required: "Password is required" })}
+        />
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="phone" className="block font-bold mb-2">
+          Phone
+        </label>
+        <input
+          type="tel"
+          id="phone"
+          className="w-full px-3 py-2 border border-gray-300 rounded"
+          {...register("phone", {
+            required: "Phone number is required",
+            pattern: {
+              value: /^\d{10}$/,
+              message: "Invalid phone number",
+            },
+          })}
+        />
+        {errors.phone && <p className="text-red-500">{errors.phone.message}</p>}
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="domains" className="block font-bold mb-2">
+          Domains
+        </label>
+        <div>
+          <input
+            type="text"
+            id="domains.0.name"
+            className="w-full px-3 py-2 border border-gray-300 rounded mb-2"
+            placeholder="Domain Name"
+            {...register("domains.0.name", {
+              required: watchedFields?.domains?.some((field) => !!field),
+            })}
+          />
+          {errors.domains?.[0]?.name && (
+            <p className="text-red-500">{errors.domains[0].name.message}</p>
+          )}
+          <input
+            type="date"
+            id="domains.0.purchasedAt"
+            className="w-full px-3 py-2 border border-gray-300 rounded mb-2"
+            {...register("domains.0.purchasedAt", {
+              required: watchedFields?.domains?.some((field) => !!field),
+            })}
+          />
+          {errors.domains?.[0]?.purchasedAt && (
+            <p className="text-red-500">
+              {errors.domains[0].purchasedAt.message}
+            </p>
+          )}
+          <input
+            type="number"
+            id="domains.0.price"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
+            {...register("domains.0.price", {
+              required: watchedFields?.domains?.some((field) => !!field),
+            })}
+          />
+          {errors.domains?.[0]?.price && (
+            <p className="text-red-500">{errors.domains[0].price.message}</p>
+          )}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="hosting" className="block font-bold mb-2">
+          Hosting
+        </label>
+        <div>
+          <input
+            type="date"
+            id="hosting.0.purchasedAt"
+            className="w-full px-3 py-2 border border-gray-300 rounded mb-2"
+            {...register("hosting.0.purchasedAt", {
+              required: watchedFields?.hosting?.some((field) => !!field),
+            })}
+          />
+          {errors.hosting?.[0]?.purchasedAt && (
+            <p className="text-red-500">
+              {errors.hosting[0].purchasedAt.message}
+            </p>
+          )}
+          <input
+            type="number"
+            id="hosting.0.price"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
+            {...register("hosting.0.price", {
+              required: watchedFields?.hosting?.some((field) => !!field),
+            })}
+          />
+          {errors.hosting?.[0]?.price && (
+            <p className="text-red-500">{errors.hosting[0].price.message}</p>
+          )}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="dashboard" className="block font-bold mb-2">
+          Dashboard
+        </label>
+        <div>
+          <input
+            type="text"
+            id="dashboard.0.loginUrl"
+            className="w-full px-3 py-2 border border-gray-300 rounded mb-2"
+            placeholder="Login URL"
+            {...register("dashboard.0.loginUrl", {
+              required: watchedFields?.dashboard?.some((field) => !!field),
+            })}
+          />
+          {errors.dashboard?.[0]?.loginUrl && (
+            <p className="text-red-500">
+              {errors.dashboard[0].loginUrl.message}
+            </p>
+          )}
+          <input
+            type="text"
+            id="dashboard.0.dashboardId"
+            className="w-full px-3 py-2 border border-gray-300 rounded mb-2"
+            placeholder="Dashboard ID"
+            {...register("dashboard.0.dashboardId", {
+              required: watchedFields?.dashboard?.some((field) => !!field),
+            })}
+          />
+          {errors.dashboard?.[0]?.dashboardId && (
+            <p className="text-red-500">
+              {errors.dashboard[0].dashboardId.message}
+            </p>
+          )}
+          <input
+            type="password"
+            id="dashboard.0.password"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
+            placeholder="Dashboard Password"
+            {...register("dashboard.0.password", {
+              required: watchedFields?.dashboard?.some((field) => !!field),
+            })}
+          />
+          {errors.dashboard?.[0]?.password && (
+            <p className="text-red-500">
+              {errors.dashboard[0].password.message}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="cpanel" className="block font-bold mb-2">
+          cPanel
+        </label>
+        <div>
+          <input
+            type="text"
+            id="cpanel.0.cpanelId"
+            className="w-full px-3 py-2 border border-gray-300 rounded mb-2"
+            placeholder="cPanel ID"
+            {...register("cpanel.0.cpanelId", {
+              required: watchedFields?.cpanel?.some((field) => !!field),
+            })}
+          />
+          {errors.cpanel?.[0]?.cpanelId && (
+            <p className="text-red-500">{errors.cpanel[0].cpanelId.message}</p>
+          )}
+          <input
+            type="password"
+            id="cpanel.0.password"
+            className="w-full px-3 py-2 border border-gray-300 rounded"
+            placeholder="cPanel Password"
+            {...register("cpanel.0.password", {
+              required: watchedFields?.cpanel?.some((field) => !!field),
+            })}
+          />
+          {errors.cpanel?.[0]?.password && (
+            <p className="text-red-500">{errors.cpanel[0].password.message}</p>
+          )}
+        </div>
+      </div>
+      <button type="submit" className="p-[3px] relative  w-full">
+        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+        <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+          Create User
+        </div>
+      </button>
+    </form>
+  );
+};
+
+export default CreateUserform;
